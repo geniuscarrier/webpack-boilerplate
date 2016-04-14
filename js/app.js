4023615e3bad3af5ad0aa7137b3f510afcf13e14
Yang Zhao <geniuscarrier@gmail.com>
2016-04-14 16:03:18 -0700

    Add example page

--- js/app.js
+++ js/app.js
@@ -4,4 +4,29 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'waypoints';
 import 'scrollTo';
 
-console.log('Webpack boilerplate starting...');
+/**
+ * EXAMPLE JS STARTS
+ */
+$(function() {
+    $('[id^=scrollTo]').click(function() {
+        var id = $(this).attr('id').slice(9);
+        $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
+    });
+
+    $('#marketing').waypoint(function() {
+        $('.img-circle').addClass('animated zoomIn');
+    }, {
+        offset: '50%',
+        triggerOnce: true
+    });
+
+    $('.featurette').waypoint(function() {
+        $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
+    }, {
+        offset: '50%',
+        triggerOnce: true
+    });
+});
+/**
+ * EXAMPLE JS ENDS
+ */
